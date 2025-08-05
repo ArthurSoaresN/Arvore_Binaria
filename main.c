d45335bc544c34be44a7691632b1c12bcb77091e
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-05 00:51:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h> // Para malloc
 
+// A razão pela qual usamos ponteiros na definição da 
+// struct é para permitir a auto-referência de forma 
+// indireta e a alocação dinâmica de memória.
+
+typedef struct {
+	int key;
+	No* No_Esquerdo;
+	No* No_Direito;
+} No;
 
 
 
