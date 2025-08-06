0168c7512427c0dc2288e15b12d34296edbfb994
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-06 20:59:56 -0300

    Update avl.c

--- avl.c
+++ avl.c
@@ -5,8 +5,26 @@
  
  Arvore AVL (Adelson Velskii e Landis) criada por eles em 1962
  Permite rebalanceamento local, apenas na parte afetada
- pela "inserção" ou "remoção", usando rotação simples ou dupla
+ pela "inserção" ou "remoção", usando rotação simples ou dupla.
  
+ A arvore AVL busca manter-se como uma arvore binária quase
+ completa (balanceada) "quase cheia".
+ 
+ Calculo: fator de balanceamento
+ Custo do algoritmo máximo é O(logN).
+ 
+ Fator de balanceamento ou fb:
+ é a diferença nas alturas da sub-arvores esquerda e direita
+ Se uma das sub-arvores não existir sua altura será -1.
+ 
+ O fb deve ser +1, 0 ou -1 para ser uma arvore balanceada.
+ Nó com fb desbalanceado é o pivô e precisa ser balanceado.
+ 
+ filhos a esquerda (+1)
+ filhos a direita (-1)
+ dois ou nenhum filho (0)
+ 
+ fb = altura da sub arvore esquerda - sub arvore direita
  
  
  */
