21973b8926e01de0d32aee88dcc3d856916e4ccd
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-06 20:52:52 -0300

    Update avl.c

--- avl.c
+++ avl.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ 
+ Arvore AVL (Adelson Velskii e Landis) criada por eles em 1962
+ Permite rebalanceamento local, apenas na parte afetada
+ pela "inserção" ou "remoção", usando rotação simples ou dupla
+ 
+ 
+ 
+ */
 
 int main()
 {
