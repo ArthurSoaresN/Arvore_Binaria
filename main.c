b6507c716c1f951da8ef53f029203d76aadc36b0
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-05 22:36:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 // struct é para permitir a auto-referência de forma 
 // indireta e a alocação dinâmica de memória.
 
-typedef struct {
+typedef struct No {
 	int key;
 	No* No_Esquerdo; // Ponteiro para o No Esquerdo (Guarda o endereço de memória de outro nó)
 	No* No_Direito;  // Ponteiro para o No Direito (Possibilita a compilação e serve de "caso base")
