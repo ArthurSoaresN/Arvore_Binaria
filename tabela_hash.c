dd90582ade227969c9cb5206c902da00d7d2dae5
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-11 23:44:15 -0300

    Update tabela_hash.c

--- tabela_hash.c
+++ tabela_hash.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv)
+#define TAMANHO 31
+
+// Zera todos os indices da tabela
+void IniciarTabela(int tabela[]) {
+	for (int i = 0; i < TAMANHO; i++){
+			tabela[i] = 0
+		}
+}
+
+int main()
 {
+	// M = 2 * numero de elementos -> Número primo mais próximo;
+	// 2*15 = 30 -> M = 31
+	int tabela[TAMANHO]; 
 	
 	return 0;
 }
